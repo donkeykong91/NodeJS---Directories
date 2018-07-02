c9b6e33e32df3ea1dd2af4277e2af74a654db20c
donkeykong91 <darylnator@gmail.com>
2018-07-02 00:51:00 -0700

    Added check to see if directory exists in the same folder we're in

--- directory.js
+++ directory.js
@@ -1,9 +1,13 @@
 var fs = require("fs");
 
-fs.mkdir("lib", function (err) {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log("Directory Created");
-  }
-});
+if (fs.existsSync('lib')) {
+  console.log("Directory already there");
+} else {
+  fs.mkdir("lib", function (err) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Directory Created");
+    }
+  });
+}
